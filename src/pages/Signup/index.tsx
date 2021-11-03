ce55ebbbe6a217090e979d9c379a50501ef38a3b
Piyush Garg <piyushgarg.dev@gmail.com>
2021-11-03 19:39:46 +0530

    API Implemention

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { styled } from '@mui/material/styles';
 import { Box, Paper } from '@mui/material';
 import Grid from '@mui/material/Grid';
@@ -10,6 +10,9 @@ import InputLabel from '@mui/material/InputLabel';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+
+import { useAppDispatch } from '../../redux/hooks';
+import { createSessionThunk } from '../../redux/slices/brand';
 
 import animatedImage from '../../assets/images/signup-banner-animated.gif';
 
@@ -32,64 +35,86 @@ const FormBox = styled(Box)({
   marginTop: '60px',
 });
 
-const SignupPage: React.FC = () => (
+const SizedBox = styled(Box)({
+  margin: '20px 0px',
+});
 
-  <Box>
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
-        <FormBox>
-          <StyledBlueHeading variant="h3">
-            Welcome to
-            {' '}
-            <br />
-            Migobucks Brands
-          </StyledBlueHeading>
-          <InputBox>
-            <FormControl variant="standard">
-              <InputLabel htmlFor="component-simple">Enter Full Name</InputLabel>
-              <Input id="component-simple" fullWidth />
-            </FormControl>
-          </InputBox>
-          <InputBox>
-            <FormControl variant="standard">
-              <InputLabel htmlFor="component-simple">Enter Full Number</InputLabel>
-              <Input id="component-simple" />
-            </FormControl>
-          </InputBox>
-          <InputBox>
-            <FormControl variant="standard">
-              <InputLabel htmlFor="component-simple">Email</InputLabel>
-              <Input id="component-simple" />
-            </FormControl>
-          </InputBox>
-          <InputBox>
-            <FormControl variant="standard">
-              <InputLabel htmlFor="component-simple">Brand Name</InputLabel>
-              <Input id="component-simple" />
-            </FormControl>
-          </InputBox>
-          <InputBox>
-            <FormControl variant="standard">
-              <InputLabel htmlFor="component-simple">Password</InputLabel>
-              <Input id="component-simple" />
-            </FormControl>
-          </InputBox>
-          <InputBox>
-            <FormGroup>
-              <FormControlLabel control={<Checkbox />} label="Accept our Terms and services" />
-            </FormGroup>
-            <LongButton variant="contained">Signup</LongButton>
-          </InputBox>
-        </FormBox>
-      </Grid>
-      <Grid item xs={8}>
-        <ImageBox>
-          <img src={animatedImage} width={800} height={650} alt="abc" />
-        </ImageBox>
+const SignupPage: React.FC = () => {
+  const dispatch = useAppDispatch();
 
+  const [fullName, setFullName] = useState<string>('');
+  const [fullNumber, setFullNumber] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [brandName, setBrandName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleCreateSession = useCallback(() => {
+    dispatch(createSessionThunk({
+      fullName,
+      number: fullNumber,
+      email,
+      brandName,
+      password,
+    }));
+  }, []);
+
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <FormBox>
+            <StyledBlueHeading variant="h3">
+              Welcome to
+            </StyledBlueHeading>
+            <StyledBlueHeading variant="h3">Migobucks Brands</StyledBlueHeading>
+            <SizedBox />
+            <InputBox>
+              <FormControl variant="standard">
+                <InputLabel htmlFor="input-fullname">Enter Full Name</InputLabel>
+                <Input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} id="input-fullname" fullWidth />
+              </FormControl>
+            </InputBox>
+            <InputBox>
+              <FormControl variant="standard">
+                <InputLabel htmlFor="input-fullnumber">Enter Full Number</InputLabel>
+                <Input value={fullNumber} onChange={(e) => setFullNumber(e.target.value)} id="input-fullnumber" />
+              </FormControl>
+            </InputBox>
+            <InputBox>
+              <FormControl variant="standard">
+                <InputLabel htmlFor="input-email">Email</InputLabel>
+                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="input-email" />
+              </FormControl>
+            </InputBox>
+            <InputBox>
+              <FormControl variant="standard">
+                <InputLabel htmlFor="input-brand">Brand Name</InputLabel>
+                <Input type="text" value={brandName} onChange={(e) => setBrandName(e.target.value)} id="input-brand" />
+              </FormControl>
+            </InputBox>
+            <InputBox>
+              <FormControl variant="standard">
+                <InputLabel htmlFor="input-passowrd">Password</InputLabel>
+                <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="input-password" />
+              </FormControl>
+            </InputBox>
+            <InputBox>
+              <FormGroup>
+                <FormControlLabel control={<Checkbox />} label="Accept our Terms and services" />
+              </FormGroup>
+              <LongButton onClick={handleCreateSession} type="submit" variant="contained">Signup</LongButton>
+            </InputBox>
+          </FormBox>
+        </Grid>
+        <Grid item xs={8}>
+          <ImageBox>
+            <img src={animatedImage} width={800} height={650} alt="abc" />
+          </ImageBox>
+
+        </Grid>
       </Grid>
-    </Grid>
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default SignupPage;
--- src/services/onboarding/index.ts
+++ src/services/onboarding/index.ts
@@ -1,7 +1,30 @@
 import { onboardApiInstance } from '../../api';
 
+export interface ICreateSessionPayload {
+  fullName: string;
+  number: string;
+  email: string;
+  brandName: string;
+  password: string
+}
+
 export const getBrandDetails = async (brandId: string) => {
   const { data } = await onboardApiInstance.get(`/${brandId}`);
 };
 
-export const x = 1;
+export const createSession = async (payload: ICreateSessionPayload) => {
+  const {
+    email, number, brandName, password, fullName,
+  } = payload;
+  const reqParam = {
+    EmailId: email,
+    PhoneNumber: number,
+    BrandName: brandName,
+    AccountPassword: password,
+    UserName: fullName,
+  };
+
+  const { data } = await onboardApiInstance.post('/', reqParam);
+  console.log('DATA', data);
+  return data;
+};
