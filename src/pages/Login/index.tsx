f452e016c219578d3e9547debe15876bbb87a60d
rajatdua77 <rajatdua777737@gmail.com>
2021-11-12 12:37:22 +0530

    Login Page Changes

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { styled } from '@mui/material/styles';
 import { Box } from '@mui/material';
 import Grid from '@mui/material/Grid';
@@ -8,17 +8,20 @@ import Typography from '@mui/material/Typography';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import Checkbox from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Link } from 'react-router-dom';
+import { useAppDispatch } from '../../redux/hooks';
+// import { createSessionThunk } from '../../redux/slices/brand';
+
 import animatedImage from '../../assets/images/login-animated-image.gif';
 
 const StyledBlueHeading = styled(Typography)({
   color: '#000000',
-
 });
 
 const LongButton = styled(Button)({
-  width: '250px',
+  width: '270px',
+  marginTop: '10px',
 });
 const ImageBox = styled(Box)({
   marginTop: '30px',
@@ -28,47 +31,69 @@ const InputBox = styled(Box)({
 });
 const FormBox = styled(Box)({
   marginLeft: '150px',
-  marginTop: '150px',
+  marginTop: '60px',
 });
-const LoginPage: React.FC = () => (
-  <Box>
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
-        <FormBox>
-          <StyledBlueHeading variant="h3">
-            Welcome to
-            {' '}
-            <br />
-            Migobucks Brands
-          </StyledBlueHeading>
-          <InputBox>
-            <FormControl variant="standard" sx={{ m: 1, width: '25ch' }}>
-              <InputLabel htmlFor="component-simple">Email</InputLabel>
-              <Input id="component-simple" />
-            </FormControl>
-          </InputBox>
-          <InputBox>
-            <FormControl variant="standard" sx={{ m: 1, width: '25ch' }}>
-              <InputLabel htmlFor="component-simple">Password</InputLabel>
-              <Input id="component-simple" />
-            </FormControl>
-          </InputBox>
-          <InputBox>
-            <FormGroup>
-              <FormControlLabel control={<Checkbox sx={{ fontSize: 15 }} />} label="Accept our Terms and services" />
-            </FormGroup>
-            <LongButton variant="contained">Signin</LongButton>
-          </InputBox>
-        </FormBox>
-      </Grid>
-      <Grid item xs={8}>
-        <ImageBox>
-          <img src={animatedImage} width={800} height={650} alt="abc" />
-        </ImageBox>
 
+const SizedBox = styled(Box)({
+  margin: '20px 0px',
+});
+const CheckboxLink = styled(Link)({
+  fontSize: '14px',
+});
+
+const LoginPage: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const breakPointMobile = useMediaQuery('(max-device-width: 480px)');
+
+  // const handleCreateSession = useCallback(() => {
+  //   dispatch(createSessionThunk({
+  //     email,
+  //     password,
+  //   }));
+  // }, []);
+
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <FormBox sx={{ marginLeft: breakPointMobile ? '45px' : '150px' }}>
+            <StyledBlueHeading variant="h3" sx={{ fontSize: breakPointMobile ? '30px' : '' }}>
+              Welcome to
+            </StyledBlueHeading>
+            <StyledBlueHeading variant="h3" sx={{ fontSize: breakPointMobile ? '30px' : '' }}>Migobucks Brands</StyledBlueHeading>
+            <SizedBox />
+            <InputBox>
+              <FormControl variant="standard" sx={{ width: '25ch' }}>
+                <InputLabel htmlFor="input-email">Email</InputLabel>
+                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="input-email" required />
+              </FormControl>
+            </InputBox>
+            <InputBox>
+              <FormControl variant="standard" sx={{ width: '25ch' }}>
+                <InputLabel htmlFor="input-passowrd">Password</InputLabel>
+                <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="input-password" required />
+              </FormControl>
+            </InputBox>
+            <Checkbox size="small" />
+            <CheckboxLink to="/"> Accept our Terms and services </CheckboxLink>
+            <LongButton variant="contained">Signin</LongButton>
+          </FormBox>
+        </Grid>
+        {!breakPointMobile && (
+          <Grid item xs={8}>
+            <ImageBox>
+              <img src={animatedImage} width={900} height={600} alt="abc" />
+            </ImageBox>
+
+          </Grid>
+        )}
       </Grid>
-    </Grid>
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default LoginPage;
