1aae595032f070236c8b03e0132d8b59ec2a53db
Prasanth M S <servers@migobucks.com>
2022-01-10 14:46:09 +0530

    Product List changes

--- src/pages/Inventary/Index.tsx
+++ src/pages/Inventary/Index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../../components/Dashboard/Layout';
+import ProductList from '../../components/invenatory/ProductList';
 import MyProducts from '../../components/invenatory/MyProducts';
 
 const Inventory: React.FC = () => (
   <Layout>
-    <MyProducts />
+    <ProductList />
   </Layout>
 );
 
